51e86c17350eed1932a87fe2a6ad28cc3ae11647
Nate Babyak <nate.babyak@outlook.com>
2025-12-28 18:53:50 -0500

    add dialog/drawer for image upload button

--- app/upload/map-button-group.tsx
+++ app/upload/map-button-group.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ButtonGroup } from "@/components/ui/button-group";
-import { Check, Locate, Navigation } from "lucide-react";
+import { Check, Locate, Navigation, X } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Coordinates } from "@/lib/math";
 import { createClient } from "@/lib/supabase/client";
@@ -10,15 +10,34 @@ import {
   DEFAULT_ZOOM,
 } from "@/lib/constants";
 import { toast } from "sonner";
-import { useRouter } from "next/navigation";
 import { useMap } from "react-map-gl/maplibre";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { useState } from "react";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "@/components/ui/dialog";
+import { Spinner } from "@/components/ui/spinner";
+import { DialogTitle, DialogTrigger } from "@radix-ui/react-dialog";
+import { useRouter } from "next/navigation";
 
 interface MapButtonGroupProps {
   markerCoordinates: Coordinates | null;
   photo: File | null;
 }
 
-interface SubmitButtonProps {
+interface SubmitDialogProps {
   markerCoordinates: Coordinates | null;
   photo: File | null;
 }
@@ -37,7 +56,7 @@ export function MapButtonGroup({
         <NavigationButton />
       </ButtonGroup>
       <ButtonGroup>
-        <SubmitButton markerCoordinates={markerCoordinates} photo={photo} />
+        <SubmitDialog markerCoordinates={markerCoordinates} photo={photo} />
       </ButtonGroup>
     </ButtonGroup>
   );
@@ -93,19 +112,16 @@ function NavigationButton() {
   );
 }
 
-function SubmitButton({ markerCoordinates, photo }: SubmitButtonProps) {
+function SubmitDialog({ markerCoordinates, photo }: SubmitDialogProps) {
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const isMobile = useIsMobile();
   const router = useRouter();
 
   const handleClick = async () => {
-    if (!photo) {
-      toast.error("No Photo Uploaded");
-      return;
-    }
+    if (!photo || !markerCoordinates) return;
 
-    if (!markerCoordinates) {
-      toast.error("No Marker Placed");
-      return;
-    }
+    setLoading(true);
 
     const supabase = createClient();
     const uuid = crypto.randomUUID();
@@ -118,9 +134,9 @@ function SubmitButton({ markerCoordinates, photo }: SubmitButtonProps) {
       });
 
     if (storageError) {
-      toast.error("Something Went Wrong", {
-        description: storageError.message,
-      });
+      toast.error("Something Went Wrong");
+      setLoading(false);
+      setOpen(false);
       return;
     }
 
@@ -136,17 +152,123 @@ function SubmitButton({ markerCoordinates, photo }: SubmitButtonProps) {
 
     if (insertError) {
       toast.error("Something Went Wrong");
+      setLoading(false);
+      setOpen(false);
       return;
     }
 
     toast.success("Photo Uploaded Successfully");
-    router.push("/");
+
+    router.replace("/");
   };
 
+  if (isMobile) {
+    return (
+      <Drawer onOpenChange={setOpen} open={open}>
+        <DrawerTrigger asChild>
+          <Button className="rounded-full">
+            <Check />
+            Submit
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Upload Photo</DrawerTitle>
+          </DrawerHeader>
+          <Content />
+          <DrawerFooter>
+            <ButtonGroup className="ml-auto">
+              <ButtonGroup>
+                <SubmitButton />
+              </ButtonGroup>
+              <ButtonGroup>
+                <DialogClose asChild>
+                  <CancelButton />
+                </DialogClose>
+              </ButtonGroup>
+            </ButtonGroup>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
   return (
-    <Button onClick={handleClick} className="rounded-full">
-      <Check />
-      Submit
-    </Button>
+    <Dialog onOpenChange={setOpen} open={open}>
+      <DialogTrigger asChild>
+        <Button className="rounded-full">
+          <Check />
+          Submit
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Upload Photo</DialogTitle>
+        </DialogHeader>
+        <Content />
+        <DialogFooter>
+          <ButtonGroup className="ml-auto">
+            <ButtonGroup>
+              <SubmitButton />
+            </ButtonGroup>
+            <ButtonGroup>
+              <DialogClose asChild>
+                <CancelButton />
+              </DialogClose>
+            </ButtonGroup>
+          </ButtonGroup>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
+
+  function Content() {
+    return (
+      <div className="flex flex-col gap-4 px-2">
+        <div
+          className={cn(
+            "flex items-center gap-2",
+            photo && "text-muted-foreground"
+          )}
+        >
+          {photo ? <Check /> : <X />}
+          Photo Uploaded
+        </div>
+        <div
+          className={cn(
+            "flex items-center gap-2",
+            markerCoordinates && "text-muted-foreground"
+          )}
+        >
+          {markerCoordinates ? <Check /> : <X />}
+          Marker Placed
+        </div>
+      </div>
+    );
+  }
+
+  function SubmitButton() {
+    return (
+      <Button
+        disabled={loading || !photo || !markerCoordinates}
+        onClick={handleClick}
+      >
+        {loading ? <Spinner /> : <Check />}
+        Submit
+      </Button>
+    );
+  }
+
+  function CancelButton() {
+    return (
+      <Button
+        disabled={loading}
+        onClick={() => setOpen(false)}
+        variant="outline"
+      >
+        <X />
+        Cancel
+      </Button>
+    );
+  }
 }
