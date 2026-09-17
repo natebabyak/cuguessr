9a13aaf1413cf338c2cd68d5a28258f570b409f4
Nate Babyak <nate.babyak@outlook.com>
2026-09-16 20:05:44 -0400

    Add Vercel Analytics to the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "photoswipe/style.css";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -42,6 +43,7 @@ export default async function RootLayout({ children }: LayoutProps<"/">) {
             {children}
             <Toaster />
           </ThemeProvider>
+          <Analytics />
         </body>
       </html>
       <SpeedInsights />
